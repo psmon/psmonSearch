fcde1d346c23041b53b30b3714c5af89d0ba4ff1
psmon <psmon@live.co.kr>
2017-03-05 17:16:52 +0900

    TestView 업그레이드

--- playWordParser/public/javascripts/websockModule.js
+++ playWordParser/public/javascripts/websockModule.js
@@ -4,7 +4,7 @@ $(function () {
     var content = $('#content');
     var input = $('#input');
     var status = $('#status');
-    var myName = false;
+    var myName = "test";
     var author = null;
     var logged = false;
     var socket = atmosphere;
@@ -12,23 +12,24 @@ $(function () {
     var transport = 'websocket';
 
     var request = { url: 'ws://' + window.location.host +'/wsapi',
-    contentType : "application/json",
-    logLevel : 'debug',
-    transport : transport ,
-    enableProtocol : true,
-    trackMessageLength : true,
-    reconnectInterval : 5000,
-    fallbackTransport: 'long-polling'};
+        contentType : "application/json",
+        logLevel : 'debug',
+        transport : transport ,
+        enableProtocol : false,
+        trackMessageLength : true,
+        reconnectInterval : 5000,
+        fallbackTransport: 'long-polling'
+    };
 
     request.onOpen = function(response) {
+        console.log('ttt');
         content.html($('<pre>', { text: 'Atmosphere connected using ' + response.transport + '\r\n' +
-            'Test Sample' + '\r\n' +
-            '{ "operation": "AUTHENTICATE",   "values": ["xxxxxxxxxxxx"] }' + '\r\n' +
-            '{ "operation": "SUBSCRIBE",   "values":   ["poker.tournaments.sng","poker.tournaments.scheduled"] }' + '\r\n' +
-            '{ "operation": "SUBSCRIBE",   "values": ["poker.tournaments.registered"] }' + '\r\n'
+            '형태소 분석기 테스트툴' + '\r\n' +
+            '채팅에 입력되는 한글문장의 형태소를 실시간 분석합니다.' + '\r\n' +
+            '사용라이브러리 : 꼬꼬마 (http://kkma.snu.ac.kr/documents/)' + '\r\n'
         }));
         input.removeAttr('disabled').focus();
-        status.text('Choose name:');
+        status.text('test:');
         transport = response.transport;
     };
 
@@ -36,6 +37,7 @@ $(function () {
         input.removeAttr('disabled').focus();
         content.html($('<p>', { text: 'Atmosphere re-connected using ' + response.transport }));
     };
+
     <!-- For demonstration of how you can customize the fallbackTransport using the onTransportFailure function -->
     request.onTransportFailure = function(errorMsg, request) {
         atmosphere.util.info(errorMsg);
@@ -44,6 +46,7 @@ $(function () {
         }
         header.html($('<h3>', { text: 'LobbyAPI WebSocket TestPage, Default transport is WebSocket, fallback is ' + request.fallbackTransport }));
     };
+
     request.onMessage = function (response) {
         var message = response.responseBody;
         try {
@@ -54,66 +57,38 @@ $(function () {
             return;
         }
         input.removeAttr('disabled').focus();
-        if (!logged && myName) {
-            logged = true;
-            status.text(myName + ': ').css('color', 'blue');
-        } else {
-            var me = json.author == author;
-            var date = typeof(json.time) == 'string' ? parseInt(json.time) : json.time;
-            if(json.author){
-                addMessage(json.author, json.message, me ? 'blue' : 'black', new Date(date));
-            }else{
-                addMessage2("serverpush", message, 'blue', parseInt(json.timestamp) );
-        }
-    }
+        addMessage2("serverpush", message, 'blue');
+
     };
-        request.onClose = function(response) {
-            content.html($('<p>', { text: 'Client closed the connection after a timeout' }));
-            subSocket.push(atmosphere.util.stringifyJSON({ author: author, message: 'disconnecting' }));
-            input.attr('disabled', 'disabled');
-        };
-        request.onError = function(response) {
-            content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
-            + 'socket or the server is down' }));
-            logged = false;
-        };
-        request.onReconnect = function(request, response) {
-        //content.html($('<p>', { text: 'Connection lost, trying to reconnect. Trying to reconnect ' + request.reconnectInterval}));
-        //input.attr('disabled', 'disabled');
-        };
-        subSocket = socket.subscribe(request);
-        input.keydown(function(e) {
-            if (e.keyCode === 13) {
-                var msg = $(this).val();
+
+
+    request.onClose = function(response) {
+        content.html($('<p>', { text: 'Client closed the connection after a timeout' }));
+        subSocket.push(atmosphere.util.stringifyJSON({ author: author, message: 'disconnecting' }));
+        input.attr('disabled', 'disabled');
+    };
+    request.onError = function(response) {
+        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
+        + 'socket or the server is down' }));
+        logged = false;
+    };
+    request.onReconnect = function(request, response) {
+    //content.html($('<p>', { text: 'Connection lost, trying to reconnect. Trying to reconnect ' + request.reconnectInterval}));
+    //input.attr('disabled', 'disabled');
+    };
+
+    subSocket = socket.subscribe(request);
+
+    input.keydown(function(e) {
+        if (e.keyCode === 13) {
+            var msg = $(this).val();
             // First message is always the author's name
             if (author == null) {
                 author = msg;
-        }
-
-        /*
-        if(msg=="1"){
-            subSocket.push(atmosphere.util.stringifyJSON({ "operation": "SUBSCRIBE", "values": ["poker.tournaments.sng"] }));
-        }else if(msg=="2"){
-            subSocket.push(atmosphere.util.stringifyJSON({ operation: "SUBSCRIBE", values: ["poker.tournaments.scheduled"] }));
-        }else if(msg=="3"){
-            subSocket.push(atmosphere.util.stringifyJSON({ operation: "SUBSCRIBE", values: "poker.tournaments.scheduled" }));
-        }else{
-            subSocket.push(atmosphere.util.stringifyJSON({ author: author, message: msg }));
-        }*/
-
-
-        var sendMsg = '{"text":"'+ msg + '","pid":"WordParserInfo","reqID":0}'
-
-
-        subSocket.push( atmosphere.util.stringifyJSON( $.parseJSON(sendMsg)  ));
-
-        $(this).val('');
-        //input.attr('disabled', 'disabled');
-        if (myName === false) {
-        myName = msg;
-        }
-
-
+            }
+            var sendMsg = '{"text":"'+ msg + '","pid":"WordParserInfo","reqID":0}'
+            subSocket.push( atmosphere.util.stringifyJSON( $.parseJSON(sendMsg)  ));
+            $(this).val('');
         }
     });
 
@@ -124,9 +99,8 @@ $(function () {
         + ': ' + message + '</p>');
     }
 
-    function addMessage2(author, message, color, datetime) {
-    content.append('<p><span style="color:' + color + '">' + author + '</span> &#64; ' +
-        + datetime
+    function addMessage2(author, message, color) {
+    content.append('<p><span style="color:' + color + '">' + author + '</span> &#64; '
         + ': ' + message + '</p>');
     }
 
